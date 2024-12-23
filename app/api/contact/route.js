b3e412a15514f756d1877908c1972a7ec48e9f06
khushalivaghani <kushalivaghani150@gmail.com>
2024-12-23 20:29:54 +0530

    Updated project files

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -6,11 +6,11 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 3000,
   secure: false, 
   auth: {
-    user: process.env.EMAIL_ADDRESS,
-    pass: process.env.GMAIL_PASSKEY, 
+    user: "meetvaghani1239@gmail.com",
+    pass: "mkmo nomf zyrk shsk", 
   },
 });
 
--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -12,7 +12,7 @@ export const projectsData = [
         id: 2,
         name: 'DocDirect- Simplifying Healthcare Appointments',
         description: 'DocDirect is an advanced healthcare platform that streamlines medical appointment bookings for users and allows doctors to manage applications efficiently, increasing user engagement by 40%. The platform includes a robust admin dashboard that enhances data retrieval speed by 50% and improves workflow efficiency by 30%. Stringent access controls and timely notifications boost data security and improve communication and tracking by 25%.',
-        tools: ['ReactJs', 'Tailwind CSS', "HTML", "CSS", "MongoDB", "Express", "Node",],
+        tools: ['ReactJs', 'Tailwind CSS', "HTML", "CSS", "MongoDB", "Express", "Node"],
         role: 'Full Stack Developer',
         code: '',
         demo: '',
@@ -28,22 +28,23 @@ export const projectsData = [
     },
     {
         id: 4,
+        name: 'Hotel Management System - Python Tkinter',
+        description: "The Hotel Management System is a desktop-based billing application designed for restaurants or hotels. It facilitates smooth billing processes by allowing staff to add items from predefined categories (e.g., Snacks, Main Course, and Hygienic Items), calculate totals, and generate a detailed bill for customers.",
+        tools: ['Python', 'Tkinter', 'Random Module', 'OS and SYS Modules', 'MessageBox (from Tkinter)', 'React Hook Form'],
+        code: '',
+        demo: '',
+        role: 'Python Developer',
+    },
+    {
+        id: 5,
         name: 'FinLink- Secure and Real-Time Financial Transactions',
         description: "FinLink is a financial SaaS platform that integrates multiple bank accounts, offering real-time transaction updates and secure money transfers. It features ultra-secure SSR authentication, Plaid integration for seamless bank account linking, and Dwolla for efficient fund transfers. The platform ensures an enhanced user experience with real-time updates and detailed transaction history. Built with responsive design, it is fully compatible across all devices.",
         tools: ['Next.js', 'TypeScript', 'Appwrite', 'Plaid', 'Dwolla', 'React Hook Form', 'Zod', 'TailwindCSS', 'Chart.js', 'ShadCN', 'SSR Authentication', 'MongoDB', 'Docker'],
         code: '',
         demo: '',
         role: 'Full Stack Developer',
-    },
-    {
-        id: 5,
-        name: 'Hotel Management System - Python Tkinter',
-        description: "The Hotel Management System is a desktop-based billing application designed for restaurants or hotels. It facilitates smooth billing processes by allowing staff to add items from predefined categories (e.g., Snacks, Main Course, and Hygienic Items), calculate totals, and generate a detailed bill for customers.",
-        tools: ['Python', 'Tkinter', 'Random Module', 'OS and SYS Modules', 'MessageBox (from Tkinter)', 'React Hook Form',],
-        code: '',
-        demo: '',
-        role: 'Python Developer',
     }
+    
 ];
 
 
