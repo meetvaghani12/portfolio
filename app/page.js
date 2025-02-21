3626e9f906d0e1bbe1ffa42faae557bc162e9060
khushalivaghani <kushalivaghani150@gmail.com>
2025-02-21 08:51:51 +0530

    client side

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
