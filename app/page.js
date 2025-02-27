e8ce15c098e20ef841d485ab5473fb66bdb9d5dc
Meet Vaghani <meetvaghani1238@gmail.com>
2025-02-27 21:32:08 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-
+"use client"
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
